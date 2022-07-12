b576d707a2a42178c418b4926df7aeb5dd5a9f1d
Mauricio Lombano <mauricio.lombano@gmail.com>
2022-07-12 13:06:36 -0500

    Ajustes

---
+++ src/pages/home.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+/// Componentes
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+const Home = () => {
+
+    return (
+        <Box>
+            <Stack direction="Row">
+                <div>
+                HOLA
+                </div>
+            </Stack>
+        </Box>
+        
+    )
+
+}
+
+export default Home
